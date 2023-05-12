64244acde35652e136bbbe2dffc2a38602d3ef95
saraoberdoerfer <sara.oberdoerfer@gritacademy.se>
2023-05-12 14:38:36 +0200

    mp1 ändring

--- java/script.js
+++ java/script.js
@@ -2,55 +2,18 @@ document.querySelector(`body`).style.textAlign = `center`;
 document.querySelector(`body`).style.marginLeft = `300px`;
 document.querySelector(`body`).style.marginRight = `300px`;
 
-const h5 = document.createElement(`h5`);
-console.log(h5);
 
-document.body.append(h5);
-h5.innerText = `Rad 1`;
 
-h5.style.color = `hsl(255, 100%, 72%)`;
-h5.style.background = `hsl(126, 100%, 80%)`;
-
-const h4 = document.createElement(`h4`);
-console.log(h4);
-
-document.body.append(h4);
-h4.innerText = `Rad 2`;
-
-h4.style.color = `hsl(255, 100%, 72%)`;
-h4.style.background = `hsl(144, 100%, 80%)`;
-h4.style.fontSize = `20px`;
-
-const h3 = document.createElement(`h3`);
-console.log(h3);
-
-document.body.append(h3);
-h3.innerText = `Rad 3`;
-
-h3.style.color = `hsl(255, 100%, 72%)`;
-h3.style.background = `hsl(155, 100%, 80%)`;
-h3.style.fontSize = `30px`;
-
-const h2 = document.createElement(`h2`);
-console.log(h2);
-
-document.body.append(h2);
-h2.innerText = `Rad 4`;
-
-h2.style.color = `hsl(255, 100%, 72%)`;
-h2.style.background = `hsl(166, 100%, 80%)`;
-h2.style.fontSize = `40px`;
-
-const h1 = document.createElement(`h1`);
-console.log(h1);
-
-document.body.append(h1);
-h1.innerText = `Rad 5`;
-
-h1.style.color = `hsl(255, 100%, 72%)`;
-h1.style.background = `hsl(190, 100%, 80%)`;
-h1.style.fontSize = `50px`;
-
+for(let i=1; i<6; i++){
+    const text = document.createElement(`p`);
+    document.body.append(text);
+    text.style.color = `hsl(255, 100%, 72%)`;
+    text.innerText = `Rad ${i}`;
+    const font = 20+(i*5);
+    text.style.fontSize = `${font}px`;
+    const hue = 126+(i*10);
+    text.style.backgroundColor = `hsl(${hue}, 100%, 80%)`;
+}
 
 const div = document.createElement(`div`);
 document.body.append(div);
